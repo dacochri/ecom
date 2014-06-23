126ea70f31022de1617facf778f819faaebb5a51
dacochri <iramortis@gmail.com>
2014-06-22 22:32:26 -0400

    basic form

--- app/views/products/_form.blade.php
+++ app/views/products/_form.blade.php
@@ -1,1 +1,15 @@
-Form for products
+{{ Form::open() }}
+  {{ Form::label('category_id', 'Category') }}
+  {{ Form::text('category_id') }}
+  {{ Form::label('name', 'Name') }}
+  {{ Form::text('name') }}
+  {{ Form::label('price', 'Price') }}
+  {{ Form::text('price') }}
+  {{ Form::label('manufacturer', 'Manufacturer') }}
+  {{ Form::text('manufacturer') }}
+  {{ Form::label('quantity', 'Quantity') }}
+  {{ Form::text('quantity') }}
+  {{ Form::label('description', 'Description') }}
+  {{ Form::text('description') }}
+  {{ Form::submit() }}
+{{ Form::close() }}
