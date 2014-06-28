619959e90e283660aa2184c5969d628765171bde
dacochri <iramortis@gmail.com>
2014-06-28 15:49:44 -0400

    create and update in same form

--- app/views/categories/form.blade.php
+++ app/views/categories/form.blade.php
@@ -1,7 +1,12 @@
-{{ Form::open(['url' => 'categories']) }}
+{{ HTML::ul($errors->all()) }}
+@if(isset($category))
+  {{ Form::model($category, ['route' => ['categories.update', $category->id], 'method' => 'PUT']) }}
+@else
+  {{ Form::open(['url' => 'categories']) }}
+@endif
   {{ Form::label('category_id', 'Category') }}
-  {{ Form::select('category_id', ['' => 'No Parent Category'] + Category::lists('name', 'id'), '') }}
+  {{ Form::select('category_id', ['' => 'No Parent Category'] + Category::lists('name', 'id'), (isset($category) ? $category->category_id : Input::old('category_id'))) }}
   {{ Form::label('name', 'Name') }}
-  {{ Form::text('name', null, array('required', 'format' => 'w+')) }}
+  {{ Form::text('name', (isset($category) ? $category->name : Input::old('name')), ['required', 'format' => 'w+']) }}
   {{ Form::submit('Submit') }}
 {{ Form::close() }}
